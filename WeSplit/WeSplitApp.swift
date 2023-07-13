67528cd4770396b90e064fac6e715bdba1daab0f
Ojidaemo <vit.martsinovich@gmail.com>
2023-07-13 15:54:09 +0300

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Vitali Martsinovich on 2023-07-13.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
